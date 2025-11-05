43b2a63af095a2728e6babf10f2f85e9a99f6878
adeshbhongale <adesh@univolute.com>
2025-11-05 09:00:25 +0530

    latest commit

---
+++ Part-6/Step-6.9/src/components/AnecdoteForm.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    dispatch(addAnecdote(content))
+    e.target.anecdote.value = ''
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <input name="anecdote" />
+      </div>
+      <button type="submit">create</button>
+    </form>
+  )
+}
+
+export default AnecdoteForm
